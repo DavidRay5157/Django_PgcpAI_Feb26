b008f7152522432084e336f08fe22c4e0d41d892
DavidRay5157 <davidraykali@gmail.com>
2026-03-16 10:36:43 +0530

    django done

--- travello/models.py
+++ travello/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Destination():
+class Destination(models.Model):
+    # this is for DB models
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    reviews = models.TextField()
+    days = models.TextField()
+    offer = models.BooleanField()
+    '''
+    # this is for basic object creation to views file
     id : int
     name : str
     desc : str
     reviews : str
     days : str
+    offer : bool
+'''
+
 
--- travello/views.py
+++ travello/views.py
@@ -4,24 +4,32 @@ from django.shortcuts import render
 from travello.models import *
 # Create your views here.
 def index(request):
+    '''
+    # this is for manual data to show in index file
     des1 = Destination()
     des1.name = "mumbai"
     des1.days = "20 days"
     des1.desc = "MumbaiKar"
     des1.reviews = "beautiful place"
+    des1.offer = True
 
     des2 = Destination()
     des2.name = "Kerela"
     des2.days = "10 days"
     des2.desc = "Gods own country"
     des2.reviews = "scenic place"
+    des2.offer = True
 
     des3 = Destination()
     des3.name = "C-DAC"
     des3.days = "6 Months"
     des3.desc = "AI"
     des3.reviews = "Need Placement"
+    des3.offer = False
 
     dest = [des1, des2, des3]
+    '''
+
+    dest = Destination.objects.all()
 
     return render(request, 'index.html',{'dest':dest})
